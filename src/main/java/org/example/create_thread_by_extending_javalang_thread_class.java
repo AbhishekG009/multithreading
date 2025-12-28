bbbaceaf57a8c04182ea0087f163d9a13d90969e
AbhishekG009 <abhishekgawas774@gmail.com>
2025-12-28 20:44:37 +0530

    first commit

---
+++ src/main/java/org/example/create_thread_by_extending_javalang_thread_class.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class create_thread extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<1000;i++)
+        {
+            System.out.println("World");
+        }
+    }
+}
---
+++ src/main/java/org/example/create_thread_by_implementing_runnable_interface.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class create_thread_by_implementing_runnable_interface {
+}
